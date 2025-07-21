932836fd24a3d0d9c94f0ea9e50500605061fcdf
Durgesh14033 <durgesh.kumar@teamcomputers.com>
2025-07-21 22:12:03 +0530

    largest element in array completed

---
+++ level1/Arrays/largestElementInArray.js
@@ -0,0 +1,14 @@
+let array = [2,1,9,5,6,7];
+
+function maxValue(array){
+    let maxValue = array[0];
+    for(let i=1;i<array.length;i++){
+        if(array[i] > maxValue){
+            maxValue = array[i];
+        }
+    }
+    return maxValue;
+}
+
+let maxValueOfArray = maxValue(array);
+console.log(`Max value of the Array is ${maxValueOfArray}`);
