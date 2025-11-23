4ae15d8885d41eacd4051b5c9c07abeddf4f421b
Durgesh14033 <kumardurgesh581@gmail.com>
2025-11-24 00:30:32 +0530

    insertion sorting recap done

--- level1/Sorting/insertionSort.js
+++ level1/Sorting/insertionSort.js
@@ -9,8 +9,24 @@ class InsertionSort{
             }
         }
     }
+
+    insertionSort2(arr){
+        for(let i = 1; i < arr.length -1; i++){
+            let curr = arr[i];
+            let j = i-1;
+
+            while(j >= 0 && arr[j] > curr){
+                arr[j+1] = arr[j]
+                j--;
+            }
+            arr[j+1]=curr;
+        }
+        return arr;
+    }
 }
 let arr = [1,5,4,3,2,1];
+let arr2 = [4,3,5,2,1,6];
 let b1 = new InsertionSort();
-b1.insertionSort(arr);
-console.log(arr);
+// b1.insertionSort(arr);
+let res = b1.insertionSort2(arr2)
+console.log(res);
